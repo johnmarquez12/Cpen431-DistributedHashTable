a91f9cadaac36a788aca0ecea705b1a044d50c3c
John Marquez <johnmarquez@Johns-MacBook-Pro.local>
2023-02-25 19:10:35 -0800

    init internal client

---
+++ src/main/java/com/g10/CPEN431/A6/InternalClient.java
@@ -0,0 +1,68 @@
+package com.g10.CPEN431.A6;
+
+import java.io.IOException;
+import java.net.*;
+
+public class InternalClient {
+
+    /**
+     * @param payload note this payload will be probably be a new protobuf or Message
+     * @
+     */
+    public record InternalRequest(byte[] payload, InetAddress nodeAddress, int nodePort) { }
+
+    private final InternalRequest internalRequest;
+    private static final int TIMEOUT = 100;
+    private static final int MAX_TIMEOUT = 1000;
+    private static final int NUM_RETRIES = 3;
+
+    public InternalClient(InternalRequest internalRequest) {
+        this.internalRequest = internalRequest;
+    }
+
+
+    // TODO: instead of returning byte array, return a protobuf Message or new protobuf we define
+    public byte[] sendRequest() {
+        int timeout = TIMEOUT;
+
+        DatagramPacket packetToSend = new DatagramPacket(
+                internalRequest.payload,
+                internalRequest.payload.length,
+                internalRequest.nodeAddress,
+                internalRequest.nodePort
+        );
+
+        for (int i = 0; i <= NUM_RETRIES; i++) {
+            try (DatagramSocket socket = new DatagramSocket()) {
+
+                socket.setSoTimeout(timeout);
+                socket.send(packetToSend);
+
+                byte[] buffer = new byte[16384];
+                DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+
+
+                socket.receive(receivePacket);
+                // TODO: add reply checksum check
+//                if (verifyInternalResponseValid())
+//                    return receivePacket.getData();
+
+                return receivePacket.getData();
+            } catch (SocketTimeoutException soe) {
+                timeout = Math.min(timeout * 2, MAX_TIMEOUT);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return new byte[0];
+    }
+
+
+    // TODO: verify checksum
+//    private boolean verifyInternalResponseValid() {
+//        return true;
+//    }
+
+
+}
